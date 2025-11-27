0974777fc1581aaaf15cebf5dabba03c11f81c0e
jasser <jasserhav@gmail.com>
2025-11-27 13:29:16 +0100

    ngrok testing

--- src/main/java/tn/esprit/controllers/CategoryController.java
+++ src/main/java/tn/esprit/controllers/CategoryController.java
@@ -41,6 +41,7 @@ public class CategoryController {
 
     private void loadCategories() {
         List<Category> categories = categoryService.getAll();
+        List<Category> categorietest = categoryService.getAll();
         ObservableList<Category> observableList = FXCollections.observableArrayList(categories);
         listViewCategories.setItems(observableList);
     }
